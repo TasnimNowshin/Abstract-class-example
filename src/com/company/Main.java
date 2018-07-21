d7733761db4ed3fad67fea377ca00cacff66abfe
Tasnim <nowshintasnim@gmail.com>
2018-07-21 16:04:34 +0600

    Initial comment

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        Ratri ratri = new Ratri();
+        Tazin tazin = new Tazin();
+        Maftuha maftuha = new Maftuha();
+
+        ratri.sleep();
+        ratri.works();
+
+        tazin.sleep();
+        tazin.works();
+
+        maftuha.sleep();
+        maftuha.works();
+    }
+}
